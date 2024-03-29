dd41fd06cfb325dec9d8d34fc736bd9502e61f83
Blake Arnold <barnold@blakearnold.me>
2024-03-28 20:21:00 -0400

    Cleaning up auth exports and hooks

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,31 @@
-
-import { SvelteKitAuth } from "@auth/sveltekit"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "$lib/db"
-import GitHub from "@auth/sveltekit/providers/github"
-import azure_ad from "@auth/sveltekit/providers/azure-ad"
-import { GITHUB_ID, GITHUB_SECRET, AZURE_AD_ID, AZURE_AD_SECRET, AZURE_AD_TENANT_ID } from "$env/static/private"
+import { SvelteKitAuth } from '@auth/sveltekit';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { db } from '$lib/db';
+import GitHub from '@auth/sveltekit/providers/github';
+import azure_ad from '@auth/sveltekit/providers/azure-ad';
+import {
+	GITHUB_ID,
+	GITHUB_SECRET,
+	AZURE_AD_ID,
+	AZURE_AD_SECRET,
+	AZURE_AD_TENANT_ID
+} from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-    adapter: DrizzleAdapter(db),
-    providers: [
-        GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
-        azure_ad({ clientId: AZURE_AD_ID, clientSecret: AZURE_AD_SECRET, tenantId: AZURE_AD_TENANT_ID })
-    ],
-})
+	adapter: DrizzleAdapter(db),
+	providers: [
+		GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+		azure_ad({ clientId: AZURE_AD_ID, clientSecret: AZURE_AD_SECRET, tenantId: AZURE_AD_TENANT_ID })
+	]
+});
+
+export const authorizationHandle: Handle = async ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/chat')) {
+		const session = await event.locals.auth();
+		if (!session) {
+			throw redirect(303, '/auth/signin');
+		}
+	}
+	return resolve(event);
+};
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,5 @@
-import { redirect, type Handle } from '@sveltejs/kit';
-import { handle as authenticationHandle } from "./auth"
+import { type Handle } from '@sveltejs/kit';
+import { handle as authenticationHandle, authorizationHandle } from './auth';
 import { sequence } from '@sveltejs/kit/hooks';
-
-async function authorizationHandle({ event, resolve }) {
-    if (event.url.pathname.startsWith('/chat')) {
-        const session = await event.locals.auth();
-        if (!session) {
-            throw redirect(303, '/auth/signin');
-        }
-    }
-    return resolve(event);
-}
 
 export const handle: Handle = sequence(authenticationHandle, authorizationHandle);
